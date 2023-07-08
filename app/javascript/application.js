e0f259f6dd566fe42f047c9b9b2fe955aa66061d
Alena Bauer <alena.bauer@kleerly.de>
2023-07-08 23:29:10 +0200

    add custom JS for collapsing news content that is too large

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,3 +5,11 @@ import "@popperjs/core"
 import "bootstrap"
 import "chartkick"
 import "Chart.bundle"
+
+document.addEventListener("click", function(event) {
+    if (event.target.matches('[data-toggle="custom-collapse"]')) {
+        const target = document.querySelector(event.target.getAttribute("data-target"));
+        target.classList.toggle("full-length");
+        event.target.innerHTML = target.classList.contains("full-length") ? "Show Less" : "Show More";
+    }
+});
